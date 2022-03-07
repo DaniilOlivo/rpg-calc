cd96b7a4ed6748b063a009e0965aacc87d58b3b7
DaniilOlivo <mister.danial.olivo@gmail.com>
2022-03-07 17:10:43 +0300

    Add ping pulse

--- server/ws/index.js
+++ server/ws/index.js
@@ -13,6 +13,21 @@ const journalWs = new Journal()
 wsRouter.ws("/table", (ws, req) => {
     let socket = new Socket(ws, journalWs)
     Socket.logger("Connect")
+    
+    let liveConnect = true
+
+    setInterval(() => {
+        ws.ping()
+        liveConnect = false
+        setTimeout(() => {
+            if (!liveConnect) {
+                Socket.logger("Client died")
+                ws.close()
+            }
+        }, 30*1000)
+    }, 40 * 1000)
+
+    ws.on("pong", () => liveConnect = true)
 
     ws.on("message", (data) => {
         let package = JSON.parse(data)
