f1c7d3dd9216c0ab519b8cfe49b7893ae3e65122
DaniilOlivo <mister.danial.olivo@gmail.com>
2022-03-12 18:14:53 +0300

    Fix colors LogViewer

--- server/ws/log.js
+++ server/ws/log.js
@@ -15,32 +15,26 @@ const closeGoodbye = [
     "Кинул нас!"
 ]
 
-function packageMesHello(username) {
-    let randomIndexHello = Math.floor(Math.random() * registerHello.length)
-    let packageWs = {
-        from: username,
-        message: registerHello[randomIndexHello],
-        type: "HELLO"
-    }
-    return packageWs
-}
-
-function packageMesGoodbye(username) {
-    let randomIndexGoodbye = Math.floor(Math.random() * closeGoodbye.length)
-    let packageWs = {
-        from: username,
-        message: closeGoodbye[randomIndexGoodbye],
-        type: "GOODBYE"
-    }
-    return packageWs
-}
-
-function packageMes(username, message) {
+function baseMes(user, message, type) {
     return {
-        from: username,
-        message,
-        type: "MESSAGE"
+        from: user.username,
+        color: user.color,
+        message, type
     }
+}
+
+function packageMesHello(user) {
+    let randomIndexHello = Math.floor(Math.random() * registerHello.length)
+    return baseMes(user, registerHello[randomIndexHello], "HELLO")
+}
+
+function packageMesGoodbye(user) {
+    let randomIndexGoodbye = Math.floor(Math.random() * closeGoodbye.length)
+    return baseMes(user, closeGoodbye[randomIndexGoodbye], "GOODBYE")
+}
+
+function packageMes(user, message) {
+    return baseMes(user, message, "MESSAGE")
 }
 
 module.exports = {packageMesHello, packageMesGoodbye, packageMes}
